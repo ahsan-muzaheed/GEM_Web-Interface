47664ddcf35ba60057eccba25ab17d9ccfc6b955
JunyongLee1217 <junyong1217@gmail.com>
2021-10-18 14:23:37 -0600

    Added Binarized Option

--- python/binarize.py
+++ python/binarize.py
@@ -18,20 +18,40 @@ def pickle_save(path, data):
         pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
+def stringToInt(listInput):
+    for i in range(len(listInput)):
+        listInput[i] = int(listInput[i])
+    return listInput
+
+
+def countLabel(listInput):
+    numOfOne = 0
+    numOfZero = 0
+    for x in data:
+        if x == 1:
+            numOfOne += 1
+        if x == 0:
+            numOfZero += 1
+    print("numOfOne: ", numOfOne)
+    print("numOfZero: ", numOfZero)
+
+
 path = os.getcwd() + "/uploads/labels.npy"
 
 data = pickle_read(path)
 posVal = sys.argv[1]
-
+posVal = stringToInt(posVal.split(","))
+print("Binarized based on: ", posVal)
 # Apply positive label
 for x in range(len(data)):
-    if data[x] == int(posVal):
+
+    if data[x] in posVal:
         data[x] = 1
     else:
         data[x] = 0
-print(data[:300])
-pickle_save(path, data)
 
+pickle_save(path, data)
+countLabel(data)
 
 # newLabel = pickle_read(path)
 # print(newLabel)
