cb1e2f4df4f3202677faafbd78837273971905ee
JunyongLee1217 <junyong1217@gmail.com>
2021-11-20 18:22:39 -0700

    Updated Readme

--- python/test.py
+++ python/test.py
@@ -1,1 +1,17 @@
-print("Already have uploaded files in hands")
+newdata = []
+
+with open("requirements.txt") as f:
+
+    data = f.read()
+    data = data.split("\n")
+    for i in data:
+        if "@" not in i:
+            newdata.append(i)
+    # print(newdata)
+
+
+file = open("requirements.txt", "w")
+# print("".join(newdata) + "\n")
+for i in newdata:
+    print(i)
+    file.write(i + "\n")
